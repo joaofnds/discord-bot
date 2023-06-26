d833d982a2fb4133c7669a0e5691688879a947a2
João Fernandes <joaofnds@joaofnds.com>
2023-06-26 11:11:44 -0300

    test(reply-stupid): test all stupid words

--- src/reply-stupid.test.mjs
+++ src/reply-stupid.test.mjs
@@ -45,4 +45,29 @@ test(ReplyStupid.name, async (t) => {
 
     assert.deepEqual(message.replies, [`JsOn, ApI ${stupid}`]);
   });
+
+  await t.test("stupid words", async (t) => {
+    const testCases = [
+      ["padrao", "PaDrAo " + stupid],
+      ["padroes", "PaDrOeS " + stupid],
+      ["pattern", "PaTtErN " + stupid],
+      ["firebase", "FiReBaSe " + stupid],
+      ["simples", "SiMpLeS " + stupid],
+      ["Json", "JsOn " + stupid],
+      ["Http", "HtTp " + stupid],
+      ["Api", "ApI " + stupid],
+      ["Dto", "DtO " + stupid],
+      ["light mode", "LiGhT MoDe " + stupid],
+    ];
+
+    for (const [input, expected] of testCases) {
+      await t.test(`for '${input}' returns '${expected}'`, async () => {
+        const message = new MessageMock(input);
+
+        await new ReplyStupid().handle(message);
+
+        assert.deepEqual(message.replies, [expected]);
+      });
+    }
+  });
 });
