b27d436d6bed4f6b4fbb2bb247de713cb9b981f1
João Fernandes <joaofnds@joaofnds.com>
2023-06-27 07:57:09 -0300

    refactor: extract handler variable

--- src/main.mjs
+++ src/main.mjs
@@ -1,23 +1,20 @@
 import { Client, Events, GatewayIntentBits } from "discord.js";
+import { BotAuthorGuard } from "./bot-author-guard.mjs";
 import { preflight } from "./preflight.mjs";
 import { ReplyStupid } from "./reply-stupid.mjs";
 import { linkChain } from "./util.mjs";
-import { BotAuthorGuard } from "./bot-author-guard.mjs";
 
 preflight();
 
-const client = new Client({
+const handler = linkChain([new BotAuthorGuard(), new ReplyStupid()]);
+
+new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
   ],
-});
-
-client.on(Events.ClientReady, () => console.log("Bot is ready"));
-
-client.on(Events.MessageCreate, async (message) => {
-  await linkChain([new BotAuthorGuard(), new ReplyStupid()]).handle(message);
-});
-
-client.login(process.env.TOKEN);
+})
+  .on(Events.ClientReady, () => console.log("bot is ready"))
+  .on(Events.MessageCreate, async (message) => await handler.handle(message))
+  .login(process.env.TOKEN);
