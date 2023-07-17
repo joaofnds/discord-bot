b61950d1020f34dd521f367a1799310dab54f20b
João Fernandes <joaofnds@joaofnds.com>
2023-07-17 18:56:08 -0300

    lib: add `toMillis`

---
+++ src/lib/to-millis.mjs
@@ -0,0 +1,18 @@
+import { Millisecond, Minute, Second } from "./time.mjs";
+
+export function toMillis(amount, unit) {
+  switch (unit) {
+    case "ms":
+    case "millisecond":
+    case "milliseconds":
+      return amount * Millisecond;
+    case "s":
+    case "second":
+    case "seconds":
+      return amount * Second;
+    case "m":
+    case "minute":
+    case "minutes":
+      return amount * Minute;
+  }
+}
---
+++ src/lib/to-millis.test.mjs
@@ -0,0 +1,28 @@
+import assert from "node:assert";
+import { test } from "node:test";
+import { toMillis } from "./to-millis.mjs";
+
+test("toMillis", async (t) => {
+  const testCases = [
+    [1, "ms", 1],
+    [1, "millisecond", 1],
+    [2, "milliseconds", 2],
+    [123, "milliseconds", 123],
+
+    [1, "s", 1000],
+    [1, "second", 1000],
+    [2, "seconds", 2000],
+    [123, "seconds", 123000],
+
+    [1, "m", 60000],
+    [1, "minute", 60000],
+    [2, "minutes", 120000],
+    [123, "minutes", 7380000],
+  ];
+
+  for (const [amount, unit, expected] of testCases) {
+    await t.test(`${amount} ${unit} = ${expected}ms`, () => {
+      assert.strictEqual(toMillis(amount, unit), expected);
+    });
+  }
+});
