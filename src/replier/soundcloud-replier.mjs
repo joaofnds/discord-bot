e4ceebf0670375b18c7ce4dde2050dc3a66fe837
João Fernandes <joao.fernandes@skillshare.com>
2024-01-04 16:17:08 -0300

    fix(soundcloud-replier): remove regex global flag

--- src/replier/soundcloud-replier.mjs
+++ src/replier/soundcloud-replier.mjs
@@ -2,7 +2,7 @@ import { captures } from "../lib/captures.mjs";
 import { normalize } from "../lib/normalize.mjs";
 
 export class SoundCloudReplier {
-	regex = /([\w-]+\.mp3)/gi;
+	regex = /([\w-]+\.mp3)/i;
 
 	reply(str) {
 		const [audio] = captures(this.regex, normalize(str));
